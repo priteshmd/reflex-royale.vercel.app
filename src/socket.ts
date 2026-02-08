532794f2b4e01f0423007a3b449eb345d915c278
PRITESH <priteshmd@gmail.com>
2026-02-08 17:48:45 +0530

    changed socket url

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://YOUR_RENDER_URL.onrender.com", {
+export const socket = io("https://reflex-royale-onrender-com.onrender.com", {
   autoConnect: false,
 });
